df498a2a71e9f6b2ae9711444217ecc09d9058d8
ARTEZON <61311568+ARTEZON@users.noreply.github.com>
2023-02-18 01:34:46 +0300

    Version 1.2.5

--- TGIDownloader_EN.py
+++ TGIDownloader_EN.py
@@ -504,10 +504,13 @@ Number of images: {len(imgs)}""")
 if platform == 'win32':
     os.system('title Telegraph Image Downloader')
     if os.getcwd().lower() == os.path.join('C:', os.sep, 'Windows', 'system32').lower():
-        print('Please run the program from another directory.')
-        print('\nPress Enter to exit.')
-        wait_for_enter_key('')
-        os._exit(0)
+        try:
+            os.chdir(os.path.dirname(os.path.abspath(__file__)))
+        except Exception:
+            print('Please run the program from another directory.')
+            print('\nPress Enter to exit.')
+            wait_for_enter_key('')
+            os._exit(0)
 while True:
     try:
         os.system('cls' if platform == 'win32' else 'clear')
